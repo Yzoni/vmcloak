b1356c8fa06fe1467a2bc78b6ce0df512eb91720
Yzoni <mail@yrck.nl>
2019-11-08 14:50:05 +0100

    Init systemcall drv install

---
+++ vmcloak/dependencies/syscalldrv.py
@@ -0,0 +1,22 @@
+# Copyright (C) 2019 Y. de Boer.
+
+
+from vmcloak.abstract import Dependency
+
+class Syscalldrv(Dependency):
+    name = "syscalldrv"
+    description = "Systemwide logger for system calls"
+    default = "1.0"
+    exes = [{
+        "version": "1.0",
+        "arch": "amd64",
+        "url": "https://user.fm/files/v2-4c7e0648f2f13bf4883253b8bd87c041/syscalldrv-x64-1_0.zip",
+        "sha1": "fb2347a982da59627ed89c246ee9ebe9e43a56d6",
+    }]
+
+    def run(self):
+        self.upload_dependency("C:\\syscalldrv-x64-1_0.zip")
+        self.a.extract("C:\\syscalldrv", "C:\\syscalldrv-x64-1_0.zip")
+        
+        # Enable powershell script execution
+        self.a.execute("Set-ExecutionPolicy RemoteSigned -force")
