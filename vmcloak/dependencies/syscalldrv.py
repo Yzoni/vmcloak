042cbeb28fcec4e4983d00af481b964856a65e12
Yzoni <mail@yorickdeboer.nl>
2019-11-11 14:14:24 +0100

    Update driver

--- vmcloak/dependencies/syscalldrv.py
+++ vmcloak/dependencies/syscalldrv.py
@@ -11,7 +11,7 @@ class Syscalldrv(Dependency):
         "version": "1.0",
         "arch": "amd64",
         "url": "https://user.fm/files/v2-74df3d530cda22bef923cb70297cac05/syscalldrv-x64-1_0.zip",
-        "sha1": "4ed5bc14a0731e63f5ef24f2d65fc7b70e4a8d1",
+        "sha1": "44ed5bc14a0731e63f5ef24f2d65fc7b70e4a8d1",
     }]
 
     def run(self):
