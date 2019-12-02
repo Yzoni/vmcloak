83642f0cec6f424e0ab5eb5e0152a6dcc62b8b94
Yzoni <mail@yorickdeboer.nl>
2019-12-02 12:02:25 +0100

    update syscalldrv

--- vmcloak/dependencies/syscalldrv.py
+++ vmcloak/dependencies/syscalldrv.py
@@ -6,16 +6,16 @@ from vmcloak.abstract import Dependency
 class Syscalldrv(Dependency):
     name = "syscalldrv"
     description = "Systemwide logger for system calls"
-    default = "1.0"
+    default = "1.1"
     exes = [{
-        "version": "1.0",
+        "version": "1.1",
         "arch": "amd64",
-        "url": "https://user.fm/files/v2-f6e6222c1f1c81cb9ad02f7615401846/syscalldrv-x64-1_0.zip",
-        "sha1": "ceee00e3d204b572552c5423a3b0d11200c46cb1",
+        "url": "https://user.fm/files/v2-80282ba481f7458fb825ef60e53b929b/syscalldrv-x64-1_1.zip",
+        "sha1": "73fade332e9d22f0aa32ff471dbe14c330ef4244",
     }]
 
     def run(self):
-        self.upload_dependency("C:\\syscalldrv-x64-1_0.zip")
+        self.upload_dependency("C:\\syscalldrv-x64-1_1.zip")
 
         # Somehow cannot find the file...
         #self.a.extract("C:\\", "C:\\syscalldrv-x64-1_0.zip")
